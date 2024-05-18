5c3243acdea8ec8e96fd56b05d2b8acfd37e1b65
ptrcmnlgcs <gecosopatricia32@.gmail.com>
2024-05-18 22:27:30 +0800

    initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar">
+    <div class="container d-flex justify-content-end">
+        <ul class="nav">
+            <router-link to="/logout" class="col-6">Logout</router-link>
+        </ul> 
+    </div>
+  </nav>
+  <!-- the router view is where all the routes are rendered -->
+  <router-view/> 
+</template>
+
+<script>
+
+export default {
+    name: 'HomePage'
+}
+</script>
